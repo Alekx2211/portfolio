192e925132850b8fa249c6224efc97287824d73c
Alekx2211 <42638496+Alekx2211@users.noreply.github.com>
2022-04-07 13:03:33 +0300

    sub

---
+++ php/menu.php
@@ -0,0 +1,22 @@
+<div class="menu">
+    <div class="menu__bar">
+        <a class="menu__logo" href="index.html"><img src="img/logp.png" alt=""></a>
+        <div class="menu__nav">
+            <ul class="menu__list">
+                <li class="menu__item"><a class="menu__link active" href="index.html">Фотограф Сидорова Алена</a></li>
+                <li class="menu__item"><a class="menu__link" href="">Обо мне</a></li>
+                <li class="menu__item"><a class="menu__link" href="">Портфолио</a>
+                    <ul class="sub__list">
+                        <li class="sub__item"><a class="sub__link" href="">Фотосессия лошадей</a></li>
+                        <li class="sub__item"><a class="sub__link" href="">Фотосессия собак</a></li>
+                        <li class="sub__item"><a class="sub__link" href="">Фотосессия людей с лошадьми</a></li>
+                        <li class="sub__item"><a class="sub__link" href="">Фотосессия людей с собаками</a></li>
+                    </ul>
+                </li>
+                <li class="menu__item"><a class="menu__link" href="">Стоимость услуг</a></li>
+                <li class="menu__item"><a class="menu__link" href="">Контактная информация</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="menu__slider">
+</div><?php
