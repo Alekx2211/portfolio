8b934f851f30c12a10f12fff4c4575a2db154859
Alekx2211 <42638496+Alekx2211@users.noreply.github.com>
2022-04-15 13:49:56 +0300

    12

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+$('.sliderbig').slick({
+    dots: true,
+});
+
+$('.banner').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+
+});
+
