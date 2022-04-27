ee450473ed77a0551ede89c7352f061bdce745f8
Alekx2211 <42638496+Alekx2211@users.noreply.github.com>
2022-04-27 17:14:37 +0300

    с

---
+++ s-loshadmi.php
@@ -0,0 +1,25 @@
+<?php
+/*Template Name: С лошадьми*/
+get_header()
+?>
+<section class="section-portfolio1">
+    <div class="wrap">
+        <h1>Фото лудей с лошадьми</h1>
+        <div class="portfolio">
+            <div class="photo1"></div>
+            <div class="photo2"></div>
+            <div class="photo3"></div>
+            <div class="photo1"></div>
+            <div class="photo2"></div>
+            <div class="photo3"></div>
+        </div>
+    </div>
+</section>
+<?php
+get_footer()
+?>
+<script src="js/jquery-3.6.0.min.js"></script>
+<script src="js/sidebar.js"></script>
+<script src="js/contactform.js"></script>
+</body>
+</html>
